ef475be0e20193c159f0b7b3ceb2211924f70879
Grgur Grisogono <grgur@grgur.com>
2016-11-28 15:14:41 +0100

    Preload with Helmet

--- client/containers/App/index.js
+++ client/containers/App/index.js
@@ -1,9 +1,16 @@
 import React, { PropTypes } from 'react';
 import Toolbar from 'components/Toolbar';
+import Helmet from 'react-helmet';
 import './style.scss';
 
 const App = (props) => (
   <main className="viewport">
+    <Helmet
+      link={[
+        {href: '/0.bundle.js', rel: 'preload', as: 'script'},
+        {href: '/1.bundle.js', rel: 'preload', as: 'script'},
+      ]}
+    />
     <Toolbar />
     {props.children}
   </main>
--- webpack.config.js
+++ webpack.config.js
@@ -58,7 +58,8 @@ module.exports = {
     ],
     vendor: [
       'react',
-      'react-dom'
+      'react-dom',
+      'react-helmet'
     ]
   },
   output: {
