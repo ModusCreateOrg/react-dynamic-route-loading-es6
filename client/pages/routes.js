9b17406c5f223c94714d02ac5148c4d77bce3a09
Grgur Grisogono <grgur@grgur.com>
2016-11-29 13:25:59 +0100

    bundled routes

--- client/pages/routes.js
+++ client/pages/routes.js
@@ -1,39 +1,22 @@
 import App from 'containers/App';
-
-function errorLoading(err) {
-  console.error('Dynamic page loading failed', err);
-}
-
-function loadRoute(cb) {
-  return (module) => cb(null, module.default);
-}
+import Home from 'pages/Home';
+import Blog from 'pages/Blog';
+import About from 'pages/About';
 
 export default {
   component: App,
   childRoutes: [
     {
       path: '/',
-      getComponent(location, cb) {
-        System.import('pages/Home')
-          .then(loadRoute(cb))
-          .catch(errorLoading);
-      }
+      component: Home,
     },
     {
       path: 'blog',
-      getComponent(location, cb) {
-        System.import('pages/Blog')
-          .then(loadRoute(cb))
-          .catch(errorLoading);
-      }
+      component: Blog,
     },
     {
       path: 'about',
-      getComponent(location, cb) {
-        System.import('pages/About')
-          .then(loadRoute(cb))
-          .catch(errorLoading);
-      }
+      component: About,
     },
   ]
 };
