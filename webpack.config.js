6d2bde125eccc2cff36b8f7d5231e991b0dde6df
Grgur Grisogono <grgur@grgur.com>
2016-12-03 18:48:36 +0100

    nicer stats

--- webpack.config.js
+++ webpack.config.js
@@ -138,6 +138,19 @@ module.exports = {
     inject: true,
     port: 3000,
     compress: isProd,
-    stats: { colors: true },
+    stats: {
+      assets: true,
+      children: false,
+      chunks: false,
+      hash: false,
+      modules: false,
+      publicPath: false,
+      timings: true,
+      version: false,
+      warnings: true,
+      colors: {
+        green: '\u001b[32m',
+      }
+    },
   }
 };
